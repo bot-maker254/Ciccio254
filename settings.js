47a9c894c7baea0de812986edeb3439dd9cf4fb4
bot-maker254 <botm61676@gmail.com>
2025-05-30 02:36:24 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Chanubot',
+  author: '‎Bot',
+  botName: "Chanubot",
+  botOwner: 'stuber🥂', // Your name
+  ownerNumber: '254712889487', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "Public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.0",
+};
+
+module.exports = settings;
